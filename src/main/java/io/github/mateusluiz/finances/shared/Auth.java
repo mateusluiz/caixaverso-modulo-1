0052988589e09f0573f06f839472102e4de353a7
Mateus Luiz <mateus74890@gmail.com>
2026-01-28 09:19:56 -0300

    feat(shared): adicionar anotacao de autenticacao

---
+++ src/main/java/io/github/mateusluiz/finances/shared/Auth.java
@@ -0,0 +1,9 @@
+package io.github.mateusluiz.finances.shared;
+
+import java.util.UUID;
+
+public final class Auth {
+    private Auth() {}
+
+    public static final UUID USER_ID = UUID.fromString("00000000-0000-0000-0000-000000000001");
+}
